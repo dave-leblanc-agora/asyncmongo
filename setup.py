43c85b53089656116b0b1102f3a493d5aada0e31
Dave LeBlanc <dave.leblanc@agoramobile.com>
2015-09-16 16:01:51 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import os
 from distutils.core import setup
 
 # also update version in __init__.py
-version = '1.3'
+version = '2.0'
 
 setup(
     name="asyncmongo",
     version=version,
     keywords=["mongo", "mongodb", "pymongo", "asyncmongo", "tornado"],
-    long_description=open(os.path.join(os.path.dirname(__file__),"README.md"), "r").read(),
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read(),
     description="Asynchronous library for accessing mongodb built upon the tornado IOLoop.",
     author="Jehiah Czebotar",
     author_email="jehiah@gmail.com",
@@ -19,5 +19,4 @@ setup(
     ],
     packages=['asyncmongo', 'asyncmongo.backends'],
     requires=['pymongo (>=1.9)', 'tornado'],
-    download_url="https://bitly-downloads.s3.amazonaws.com/asyncmongo/asyncmongo-%s.tar.gz" % version,
 )
